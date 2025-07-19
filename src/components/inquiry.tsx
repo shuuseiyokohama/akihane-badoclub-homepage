27b06b657f18bcffa204f333ad5d9f52820a3595
shuuseiyokohama <shuuseiyokohama@gmail.com>
2025-07-19 10:11:26 +0900

    0719変更

--- src/components/inquiry.tsx
+++ src/components/inquiry.tsx
@@ -7,7 +7,8 @@ export const Inquiry = () =>{
     <div id="inquiry" className="inquiry">
       <h1>お問合せ</h1>
       <p>
-        お問い合わせの際は、以下のフォームに「お名前」と「バドミントン練習問合せ」の旨、返信用のメールアドレスをご記入のうえ、送信してください。
+        お問い合わせの際は、以下のフォームに項目を記載のうえ、送信してください。<br/>
+        ＊メールアドレスの記載にミスがあると連絡が取れませんので、確認のうえ入力してください。
       </p>
       <div className="inquiry-contact">
         <form
@@ -28,6 +29,31 @@ export const Inquiry = () =>{
           <div className="form-group">
             <label htmlFor="email">メールアドレス:</label>
             <input type="email" name="email" id="email" required />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="age">年齢:</label>
+            <select name="age" id="age" required>
+              <option value="">選択してください</option>
+              <option value="10s">10代</option>
+              <option value="20s">20代</option>
+              <option value="30s">30代</option>
+              <option value="40s">40代</option>
+              <option value="50s">50代</option>
+              <option value="60s">60代</option>
+              <option value="70s">70代</option>
+            </select>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="level">バドミントンのレベル:</label>
+            <select name="level" id="level" required>
+              <option value="">選択してください</option>
+              <option value="未経験">未経験</option>
+              <option value="初心者">初心者</option>
+              <option value="中級者">中級者</option>
+              <option value="上級者">上級者</option>
+            </select>
           </div>
           <button type="submit">送信</button>
         </form>
